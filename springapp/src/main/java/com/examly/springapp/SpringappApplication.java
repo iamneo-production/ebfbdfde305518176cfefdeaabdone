bc4775248f157dda551d340f5237e657d94ae589
Iamneo <techadmin@iamneo.ai>
2023-09-30 13:27:39 +0000

    Backup Commit

--- springapp/src/main/java/com/examly/springapp/SpringappApplication.java
+++ springapp/src/main/java/com/examly/springapp/SpringappApplication.java
@@ -1,19 +1,12 @@
-import org.springframework.context.ApplicationContext;
-import org.springframework.context.annotation.AnnotationConfigApplicationContext;
-import com.examly.Student; // Replace with your actual package name
+package com.shiva.StudentDB;
 
-public class MainApp {
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+@SpringBootApplication
+public class SpringApplication {
 
-    public static void main(String[] args) {
-        // Load the Spring application context
-        ApplicationContext context = new AnnotationConfigApplicationContext(AppConfig.class);
+	public static void main(String[] args) {
+		SpringApplication.run(StudentiD.class, args);
+	}
 
-        // Retrieve the Student bean
-        Student student = context.getBean("student", Student.class);
-
-        // Access student details
-        System.out.println("Student ID: " + student.getStudentId());
-        System.out.println("First Name: " + student.getFirstName());
-        System.out.println("Last Name: " + student.getLastName());
-    }
 }
