1e110038627fe1d68ee8da9406c49d8549ab4f96
Iamneo <techadmin@iamneo.ai>
2023-09-30 06:59:48 +0000

    Backup Commit

---
+++ springapp/src/main/java/com/Configuration/AppConfig.java
@@ -0,0 +1,15 @@
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@ComponentScan(basePackages = "com.yourpackage") // Replace with your actual package name
+public class AppConfig {
+
+    // Define beans here
+
+    @Bean
+    public Student student() {
+        return new Student();
+    }
+}
---
+++ springapp/src/main/java/com/examly/springapp/Student.java
@@ -0,0 +1,7 @@
+public class Student {
+    private int studentId;
+    private String firstName;
+    private String lastName;
+
+    // Constructors, getters, and setters
+}
